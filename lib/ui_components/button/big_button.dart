6e33b7cfad1577878a83140f5106703d60d0590e
DenUP <ctrike163@gmail.com>
2026-02-01 18:09:47 +0300

    bigButton

---
+++ lib/ui_components/button/big_button.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:ui_kit/ui_kit.dart';
+
+class BigButton {
+  ButtonStyle _buttonStyle() {
+    return ElevatedButton.styleFrom(
+      minimumSize: Size(335, 56),
+      padding: EdgeInsets.symmetric(horizontal: 114, vertical: 16),
+      shadowColor: Colors.transparent,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(10),
+      ),
+    );
+  }
+
+  Widget accent({
+    required String name,
+    VoidCallback? onTap,
+    bool isActive = true,
+  }) {
+    return ElevatedButton(
+      style: _buttonStyle().copyWith(
+        backgroundColor: WidgetStateProperty.all<Color>(
+          isActive ? AppColor.accent : AppColor.accentInactive,
+        ),
+        foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+      ),
+      onPressed: isActive ? onTap : null,
+      child: _text(name),
+    );
+  }
+
+  Widget noAccent({required String name, VoidCallback? onTap}) {
+    return ElevatedButton(
+      style: _buttonStyle().copyWith(
+        backgroundColor: WidgetStateProperty.all<Color>(Colors.white),
+        foregroundColor: WidgetStateProperty.all<Color>(AppColor.accent),
+      ),
+      onPressed: onTap,
+      child: _text(name),
+    );
+  }
+
+  Widget unAccent({required String name, VoidCallback? onTap}) {
+    return ElevatedButton(
+      style: _buttonStyle().copyWith(
+        backgroundColor: WidgetStateProperty.all<Color>(AppColor.inputBg),
+        foregroundColor: WidgetStateProperty.all<Color>(AppColor.black),
+      ),
+      onPressed: onTap,
+      child: _text(name),
+    );
+  }
+
+  Widget _text(String name) {
+    return Text(name, style: AppTextStyle.title3Semibold);
+  }
+}
