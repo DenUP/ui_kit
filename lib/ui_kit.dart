0f5d418c3a84fb5b90e63223041fd395b6dee9b8
DenUP <ctrike163@gmail.com>
2026-02-01 18:16:38 +0300

    small button: add

--- lib/ui_kit.dart
+++ lib/ui_kit.dart
@@ -1,4 +1,5 @@
 import 'package:ui_kit/ui_components/button/big_button.dart';
+import 'package:ui_kit/ui_components/button/small_button.dart';
 
 export 'app_text_style.dart';
 export 'app_color.dart';
@@ -6,6 +7,7 @@ export 'app_icon.dart';
 
 class UiKit {
   BigButton get bigButton => BigButton();
+  SmallButton get smallButton => SmallButton();
 }
 
 final ui = UiKit();
