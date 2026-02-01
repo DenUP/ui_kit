5699fa5b872610e42b8d9f560602cec9451596fc
DenUP <ctrike163@gmail.com>
2026-02-01 18:10:34 +0300

    uikit

--- lib/ui_kit.dart
+++ lib/ui_kit.dart
@@ -1,3 +1,11 @@
+import 'package:ui_kit/ui_components/button/big_button.dart';
+
 export 'app_text_style.dart';
 export 'app_color.dart';
 export 'app_icon.dart';
+
+class UiKit {
+  BigButton get bigButton => BigButton();
+}
+
+final ui = UiKit();
