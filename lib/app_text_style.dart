0c80abeeb6849ec5e6b4686e90a8ad1d1e650115
DenUP <ctrike163@gmail.com>
2026-02-01 17:11:46 +0300

    textstyle

---
+++ lib/app_text_style.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyle {
+  static const title1Semibold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Roboto',
+  );
+  static const title1ExtraBold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w800,
+    fontFamily: 'Roboto',
+  );
+  static const title2Regular = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+  );
+  static const title2Semibold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Roboto',
+  );
+  static const title2ExtraBold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w800,
+    fontFamily: 'Roboto',
+  );
+  static const title3Regular = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+  );
+  static const title3Medium = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Roboto',
+  );
+  static const title3Semibold = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Roboto',
+  );
+  static const headlineRegular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+  );
+  static const headlineMedium = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Roboto',
+  );
+  static const textRegular = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+  );
+  static const textMedium = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+  );
+  static const captionRegular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+  );
+  static const captionSemibold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Roboto',
+  );
+  static const caption2Regular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+  );
+  static const caption2Bold = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Roboto',
+  );
+}
