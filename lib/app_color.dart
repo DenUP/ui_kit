80f197ceb6ad82b4bce9d9c6352f0b4129650555
DenUP <ctrike163@gmail.com>
2026-02-01 17:18:21 +0300

    color

---
+++ lib/app_color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const accent = Color(0xFF2074F2);
+  static const accentInactive = Color(0xFFC5D2FF);
+  static const black = Color(0xFF2D2C2C);
+  static const white = Color(0xFFF7F7F7);
+  static const error = Color(0xFFFF4646);
+  static const success = Color(0xFF00B412);
+  static const inputBg = Color(0xFFF7F7FA);
+  static const inputStroke = Color(0xFFE6E6E6);
+  static const inputIcon = Color(0xFFBFC7D1);
+  static const placeholder = Color(0xFF98989A);
+  static const description = Color(0xFF8787A1);
+  static const cardStroke = Color(0xFFF2F2F2);
+}
